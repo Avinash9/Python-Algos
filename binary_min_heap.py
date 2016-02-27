4d14bb5617b36ac6941960610fcc71aaba96cfdb
avinash.garg <avinash.garg@inmobi.com>
2016-02-27 17:18:17 +0530

    added delete method for heap

--- binary_min_heap.py
+++ binary_min_heap.py
@@ -8,12 +8,44 @@ class BinaryMinHeap:
     def insert(self,number):
         self.heapList.append(number)
         self.currentSize = self.currentSize + 1
-        self.preup(self.currentSize)
+        self._preup(self.currentSize)
 
-    def preup(self,i):
+    def _preup(self,i):
         while i // 2 > 0:
             if self.heapList[i] < self.heapList[i // 2]:
                 tmp = self.heapList[i // 2]
                 self.heapList[i // 2] = self.heapList[i]
                 self.heapList[i] = tmp
             i //= 2
+
+    def delete(self):
+        del_val = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self._precdown(1)
+        return del_val
+
+    def _precdown(self,index):
+        while index*2 <= self.currentSize :
+            new_index = self._check_min_child(index)
+            if self.heapList[index] > self.heapList[new_index]:
+                temp = self.heapList[index]
+                self.heapList[index] =  self.heapList[new_index]
+                self.heapList[new_index] = temp
+            index = new_index
+
+
+    def _check_min_child(self,index):
+        if self.currentSize < 2*index + 1:
+            return 2*index
+        else:
+            if self.heapList[2*index] >  self.heapList[2*index+1]:
+                return 2*index +1
+            else:
+                return 2*index
+
+
+
+
+
